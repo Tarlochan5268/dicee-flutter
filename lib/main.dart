2944eefc535bba7950c899df247c489cfad3ef44
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-05-20 07:34:16 -0400

    ADDED stateful to both the dice widgets added set state and button click functionality to change dice

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,65 @@ void main() {
   );
 }
 
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int RightDiceNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            //flex: 2,
+            /* child: Image(
+              image: AssetImage('images/dice1.png'),
+            ),*/
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  if (leftDiceNumber < 6) {
+                    leftDiceNumber++;
+                  } else {
+                    leftDiceNumber = 1;
+                  }
+                });
+                print('Left Dice $leftDiceNumber Pressed');
+              }, //anonyomous function
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  if (RightDiceNumber < 6) {
+                    RightDiceNumber++;
+                  } else {
+                    RightDiceNumber = 1;
+                  }
+                });
+                print('Right Dice $RightDiceNumber Pressed');
+              },
+              child: Image(
+                image: AssetImage('images/dice$RightDiceNumber.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/*
 class DicePage extends StatelessWidget {
+  int leftDiceNumber = 1;
+  int RightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -35,7 +93,9 @@ class DicePage extends StatelessWidget {
           ),
           Expanded(
             child: FlatButton(
-              onPressed: () {},
+              onPressed: () {
+                print('Left Dice Pressed');
+              },
               child: Image(
                 image: AssetImage('images/dice1.png'),
               ),
@@ -46,3 +106,5 @@ class DicePage extends StatelessWidget {
     );
   }
 }
+
+ */
